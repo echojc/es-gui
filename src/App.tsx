4949074d987b63a826360dde638502bc44c8c772
Jonathan Chow <jonathan@movio.co>
2017-03-23 18:11:43 +1300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
 import * as React from 'react';
 
+const payload = {
+  query: {
+    bool: {
+      filter: {
+        term: {
+          fieldName: 'value',
+        },
+      },
+    },
+  },
+};
+
 export class App extends React.Component<{}, {}> {
   render() {
     return <div>Hello, world!</div>;
   }
 }
+
+type Query = Bool | Term;
+
+interface Bool {
+  bool: {
+    filter: Query | Query[];
+  };
+}
+
+interface Term {
+  term: {
+    [field: string]: string;
+  };
+}
+
+const Query = ({ query }: { query: Query }) => (
+  <div className="query">
+    {(() => { switch (true) {
+    case query.bool:
+      return <Bool bool={query.bool} />
+    }})()}
+  </div>
+);
+
+const Bool = ({ bool }: { bool: Bool }) => (
+  <div className="bool">
+  </div>
+);
+
+const Filter = ({ filter }: { filter: { filter: Query | Query[]} }) => (
+  <div className="filter">
+  </div>
+);
+
+const Term = ({ term }: { term: Term }) => (
+  <div className="filter">
+  </div>
+);
